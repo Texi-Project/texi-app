d64a24f5006a3ecc5b2aa220822d8d1410d7e9e7
Paul A. Tumwine <paulatumwine@gmail.com>
2019-12-09 23:26:45 -0600

    Setup some basic service layer classes

--- src/main/java/com/texi/app/post/service/PostService.java
+++ src/main/java/com/texi/app/post/service/PostService.java
@@ -1,7 +1,14 @@
 package com.texi.app.post.service;
 
+import com.texi.app.domain.Post;
+import com.texi.app.domain.User;
 import org.springframework.stereotype.Service;
 
-@Service
-public class PostService {
+import java.util.List;
+
+public interface PostService {
+    public void save(Post post);
+    public List<Post> findAll();
+    public void delete(Long id);
+    public Post findByUser(User user);
 }
