cdfbf6a9bc7c7f46863ced4d0680fe1b86cfc2a0
Paul A. Tumwine <paulatumwine@gmail.com>
2019-12-15 01:08:31 -0600

    Some post merge fixes

--- src/main/java/com/texi/app/Application.java
+++ src/main/java/com/texi/app/Application.java
@@ -1,19 +1,12 @@
 package com.texi.app;
 
-import com.texi.app.post.service.impl.PostServiceImpl;
-import com.texi.app.user.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.io.File;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		new File(UserController.uploadDirectory).mkdir();
-		new File(PostServiceImpl.UPLOADS_LOCATION).mkdir();
 		SpringApplication.run(Application.class, args);
 	}
 }
